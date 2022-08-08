768230aeb7bd4cccf2d6df1104d569aedb8872af
Leonid Tsaplin <leonid.tsc@gmail.com>
2022-08-08 16:24:17 +0300

    basic structure of kivy project

---
+++ main.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+class Application(App):
+    def build(self):
+        my_button = Button()
+
+
+if __name__ == '__main__':
+    Application().run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#
